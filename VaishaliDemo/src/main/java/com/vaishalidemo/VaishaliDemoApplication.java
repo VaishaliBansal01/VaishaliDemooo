c4faac637f302625cd6e5ecc89f42b9a06d21a70
VaishaliBansal01 <vaishali.bansal01@gmail.com>
2023-04-04 16:13:12 +0530

    VaishaliDemo

---
+++ VaishaliDemo/src/main/java/com/vaishalidemo/VaishaliDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vaishalidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaishaliDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaishaliDemoApplication.class, args);
+	}
+
+}
---
+++ VaishaliDemo/src/test/java/com/vaishalidemo/VaishaliDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaishalidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaishaliDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
